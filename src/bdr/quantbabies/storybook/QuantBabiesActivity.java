33eb33f5ce524538b7a77eaf20c2f9fcaf2693df
Daniel Delubac <ddelubac@gmail.com>
2012-02-29 11:06:50 -0500

    Third Commit

---
+++ src/bdr/quantbabies/storybook/QuantBabiesActivity.java
@@ -0,0 +1,13 @@
+package bdr.quantbabies.storybook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuantBabiesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
